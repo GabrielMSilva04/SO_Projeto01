d5e6ceac5dab7dfbceae6b53f5a01048fed08571
Gabriel Silva <gabrielmsilva4@ua.pt>
2023-11-02 15:41:29 +0000

    funcao print_array e opção -l

--- spacecheck.sh
+++ spacecheck.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
 
-var=""  # Variable to store the value after -n
-count=0
-
-
 # Função para calcular o espaço ocupado pelos ficheiros 
 dir=${@: -1}
-opts="$@"
+opts="$*"
 find_opts=""
 sort_cmd="sort -nr"
 
-#echo "$opts"
-#echo "$dir"
+# Function to print array elements based on start and end indices
+print_array() {
+    local -n arr=$1  # Using a nameref to reference the array
+    local start=$2
+    local end=$3
+
+    for (( i = start; i <= end-1; i++ )); do
+        echo "${arr[i]}"
+    done
+}
+
 echo "SIZE    NAME $(date +%Y%m%d) $*"
+
 # Verificar opções de seleção de ficheiros
 if [[ "$opts" == *"-n "* ]]; then #n = numerical
   name_pattern=$(echo "$opts" | sed -n 's/.*-n \([^ ]*\).*/\1/p')
@@ -30,16 +36,18 @@ if [[ "$opts" == *"-n "* ]]; then #n = numerical
   # Execute the du command and store its output into an array
   mapfile -t array < <(du "$dir" | $sort_cmd)
 
-  # Display the array elements (for demonstration)
-  for line in "${array[@]}"; do
-    echo "$line"
-  done
+  if [[ "$opts" == *"-l "* ]]; then
+    nlines=$(echo "$opts" | sed -n 's/.*-l \([^ ]*\).*/\1/p')
+  else
+    nlines=${#array[@]}
+  fi
+
+  print_array array 0 "$nlines"
 fi
   
 if [[ "$opts" == *"-d "* ]]; then
   date_limit=$(echo "$opts" | sed -n 's/.*-d \([^ ]*\).*/\1/p')
   find_opts="$find_opts -newermt $date_limit"
-
 
   # Execute the du command and store its output into an array
   mapfile -t array < <(du --time "$dir")
@@ -51,44 +59,11 @@ if [[ "$opts" == *"-d "* ]]; then
 
   echo "primeira linha:"
   echo "${array[0]}"
-
-  #echo "$date_limit"
 fi
   
 if [[ "$opts" == *"-s "* ]]; then
   size_limit=$(echo "$opts" | sed -n 's/.*-s \([^ ]*\).*/\1/p')
   find_opts="$find_opts -size +${size_limit}c"
 fi
-
-
-
-#LIXO
-for i in "$@"; do 
-  ((count++))  # Increment the counter
-
-  if [ "$i" == "-n" ] ; then
-    echo "numerical"
-    nextIndex=$((count + 1))  # Calculate the index of the next argument
-    if [ $count != 1 ] ; then
-      case $1 in
-        "-a")
-            Message="Alfabeticamente"
-            ;;
-        "-r")
-            Message="Reverso"
-            ;;
-      esac
-
-      echo "$Message"
-    fi  
-    if [ $nextIndex -le $# ]; then
-        var="${!nextIndex}"  # Get the argument at the next index
-        break  # Stop the loop as we found the value after -n
-    else
-        echo "No value found after -n" >&2
-        exit 1  # Exit with an error if there's no value after -n
-    fi
-  fi 
-done
 
 #(du "$var" | sort -n -r | cut -d '%' -f1) | grep $2)
