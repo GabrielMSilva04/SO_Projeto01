505241afc89f972f2afeda0d144d1c46228eeebf
Gabriel Silva <gabrielmsilva4@ua.pt>
2023-11-03 23:06:00 +0000

    mudança da função print_array

--- spacecheck.sh
+++ spacecheck.sh
@@ -8,14 +8,21 @@ sort_cmd="sort -nr"
 
 # Function to print array elements based on start and end indices
 print_array() {
-    local -n arr=$1  # Using a nameref to reference the array
-    local start=$2
-    local end=$3
+  local nlines
+  local -n arr=$1  # Using a nameref to reference the array
 
-    for (( i = start; i <= end-1; i++ )); do
-        echo "${arr[i]}"
-    done
+  if [[ "$opts" == *"-l "* ]]; then
+      nlines=$(echo "$opts" | sed -n 's/.*-l \([^ ]*\).*/\1/p')
+  else
+      nlines=${#arr[@]}
+  fi
+  
+  for (( i = 0; i < nlines && i < ${#arr[@]}; i++ )); do
+    trimmed_element=$(echo "${arr[i]}" | xargs)  # Trimming leading and trailing whitespace
+      echo "$trimmed_element"
+  done
 }
+
 
 if [[ "$opts" == *"-r "* ]]; then
   sort_cmd="sort -n"
@@ -37,18 +44,12 @@ if [[ "$opts" == *"-n "* ]]; then # n = filtrar nome
 
   # Find directories containing files with a specific name pattern
   mapfile -t directories < <(find "$dir" -type f -name "*$name_pattern" -exec dirname {} \; | sort -u)
-  mapfile -t array < <(du "${directories[@]}")
+  mapfile -t array < <(du "${directories[@]}" | $sort_cmd)
 
-  if [[ "$opts" == *"-l "* ]]; then
-    nlines=$(echo "$opts" | sed -n 's/.*-l \([^ ]*\).*/\1/p')
-  else
-    nlines=${#array[@]}
-  fi
-
-  print_array array 0 "$nlines"
+  print_array array
 
 
-elif [[ "$opts" == *"-d "* ]]; then
+elif [[ "$opts" == *"-d "* ]]; then #mostrar dicheiros mdificados depois da data
   # Extracting date argument
   while [[ "$#" -gt 0 ]]; do
       case $1 in
@@ -61,17 +62,10 @@ elif [[ "$opts" == *"-d "* ]]; then
   converted_date=$(date -d "$date_argument" "+%Y-%m-%d %H:%M")
 
   # Use the converted date with find -newermt to filter files
-  mapfile -t array < <(find . -type f -newermt "$converted_date" -exec du {} \;)
+  mapfile -t array < <(find . -type f -newermt "$converted_date" -exec du {} \; | $sort_cmd)
 
 
-
-  if [[ "$opts" == *"-l "* ]]; then
-    nlines=$(echo "$opts" | sed -n 's/.*-l \([^ ]*\).*/\1/p')
-  else
-    nlines=${#array[@]}
-  fi
-
-  print_array array 0 "$nlines"
+  print_array array
 
 elif [[ "$opts" == *"-s "* ]]; then
   size_limit=$(echo "$opts" | sed -n 's/.*-s \([^ ]*\).*/\1/p')
@@ -86,13 +80,5 @@ else
   # Execute the du command and store its output into an array
   mapfile -t array < <(du "$dir" | $sort_cmd)
 
-  if [[ "$opts" == *"-l "* ]]; then
-    nlines=$(echo "$opts" | sed -n 's/.*-l \([^ ]*\).*/\1/p')
-  else
-    nlines=${#array[@]}
-  fi
-
-  print_array array 0 "$nlines"
+  print_array array
 fi
-
-#(du "$dir" | sort -n -r | grep "objects")
